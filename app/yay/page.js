32bac235e884e7703c6188c32ffd3df633374be0
cerberusco <carlosjrfelices@gmail.com>
2026-02-10 23:00:05 +0100

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,8 +3,9 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'Yayyy~ lets plan a date <3'}
+      {'Menos maaaal. Te quieroooo <3'}
     </div>
   )
+
 
 }
